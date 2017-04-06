de60ab05fb4f14c932bd36acd8b335301da9d8a6
sebastianmoreno <zebaxtian@hotmail.com>
2017-04-06 09:09:26 -0500

    Se termina script para creacion de tablas

--- Docs/appShipment.sql
+++ Docs/appShipment.sql
@@ -3,8 +3,8 @@ CREATE USER AS_ADMIN IDENTIFIED BY oracle12c;
 GRANT ALL PRIVILEGES TO AS_ADMIN;
 */
 
-/* DROPS!*/
-
+/* DROPS!
+*/
 DROP TABLE transp_vehiculos;
 DROP TABLE vehiculos;
 DROP TABLE tipos_vehiculo;
@@ -13,6 +13,8 @@ DROP TABLE transportadores;
 DROP TABLE servicios;
 DROP TABLE localizaciones;
 DROP TABLE personas;
+DROP TABLE dimensiones;
+DROP TABLE paquetes;
 
 
 CREATE TABLE personas (
@@ -78,18 +80,47 @@ CREATE TABLE localizaciones (
 );
 
 CREATE TABLE servicios (
-  id_servicio  NUMERIC GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+  id_servicio  NUMERIC GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1),
   id_usuario NUMERIC,
   id_transportador NUMERIC,
   origen_lat NUMERIC(12,8),
   origen_lng NUMERIC(12,8),
   destino_lat NUMERIC(12,8),
   destino_lng NUMERIC(12,8),
+  estado VARCHAR2(2),
   fecha_inicio DATE,
   fecha_fin DATE,
-  tipo_servicio VARCHAR2(2) DEFAULT 'A' CHECK (tipo_servicio IN ('A', 'P')),
+  tipo_servicio VARCHAR2(2) DEFAULT 'A' CHECK (tipo_servicio IN ('I', 'P')),
   
+  CONSTRAINT servicios_PK PRIMARY KEY (id_servicio, id_usuario, id_transportador),
   CONSTRAINT id_usuario_FK FOREIGN KEY (id_usuario) REFERENCES personas (id_persona),
   CONSTRAINT id_transportador_FK FOREIGN KEY (id_transportador) REFERENCES personas (id_persona)
 );
 
+CREATE TABLE dimensiones (
+  id_dimensiones NUMERIC GENERATED ALWAYS AS IDENTITY (START WITH 1 INCREMENT BY 1) PRIMARY KEY,
+  volumen_min NUMERIC,
+  volumen_max NUMERIC,
+  descripcion VARCHAR(10)
+);
+
+CREATE TABLE paquetes (
+  id_paquete NUMBER,
+  id_servicio NUMERIC,
+  id_usuario NUMERIC,
+  id_transportador NUMERIC,
+  nombre_destinatario VARCHAR2(50),
+  tipo_id_destinatario VARCHAR2(2),
+  nm_id_destinatario NUMERIC,
+  tel_destinatario NUMERIC,
+  declaracion_contenido VARCHAR2(150),
+  valor_declarado NUMERIC,
+  peso NUMERIC,
+  id_dimensiones NUMBER,
+  
+  CONSTRAINT paquetes_PK PRIMARY KEY (id_paquete),
+  CONSTRAINT id_dimensiones_paquetes_FK  FOREIGN KEY (id_dimensiones) REFERENCES dimensiones (id_dimensiones),
+  CONSTRAINT id_servicio_paquetes_FK  FOREIGN KEY (id_servicio, id_usuario, id_transportador) REFERENCES servicios (id_servicio, id_usuario, id_transportador)
+);
+
+INSERT INTO personas (tipo_id, nm_id, nm_celular, nombre, apellido, email, password, fecha_registro, calificacion, activo) VALUES ('CC', 1234, 3003216598, 'Fizz', 'Seajoker', 'fizz@seajoker.com', 'fizz', null, 4.32, 'S');
