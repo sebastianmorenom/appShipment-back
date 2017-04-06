ee171c81580103f06a304e882528d7e0b29f21d1
sebastianmoreno <zebaxtian@hotmail.com>
2017-04-06 09:10:35 -0500

    1. Se agrega driver para conexión a base de datos oracle12c y se agrega configuración al application.conf
    2. Se cambia el servicio de Login a POST

--- app/controllers/Login.java
+++ app/controllers/Login.java
@@ -1,13 +1,30 @@
 package controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import model.User;
 import play.mvc.*;
 import play.libs.Json;
 
 public class Login extends Controller {
 
-    public Result login(String username){
+    public Result login(){
 
-        return ok(Json.toJson("Hello!, "+username));
+        JsonNode json = request().body().asJson();
+        System.out.println(json);
+        if(json == null) {
+            return badRequest("Expecting Json data");
+        } else {
+            User user = Json.fromJson(json, User.class);
+
+            if(user.username == null ) {
+                return badRequest("Missing parameter [username]");
+            } else if ( user.password == null ) {
+                return badRequest("Missing parameter [password]");
+            }else
+            {
+                return ok("Hello " + user.username + " - "+ user.password);
+            }
+        }
     }
 
 }
